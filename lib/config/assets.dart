b284b8677708d9e0f0a07b31430f29301ca3b2ca
Rahul Mula <96373135+mulaRahul@users.noreply.github.com>
2023-07-03 17:08:40 +0530

    ⭐ add icons

--- lib/config/assets.dart
+++ lib/config/assets.dart
@@ -1,24 +1,66 @@
-const _imgDir = "assets/img";
-
 class VuesaxIcons {
   VuesaxIcons._();
 
-  static const cogWheel = "$_imgDir/cog-wheel.svg";
-  static const edit = "$_imgDir/edit.svg";
-  static const monitor = "$_imgDir/monitor.svg";
-  static const more = "$_imgDir/more.svg";
-  static const mouse = "$_imgDir/mouse.svg";
-  static const cross = "$_imgDir/cross.svg";
-  static const chevronDown = "$_imgDir/chevron-down.svg";
-  static const alignTop = "$_imgDir/align-top.svg";
-  static const alignVertically = "$_imgDir/align-vertically.svg";
-  static const alignBottom = "$_imgDir/align-bottom.svg";
-  static const alignLeft = "$_imgDir/align-left.svg";
-  static const alignHorizontally = "$_imgDir/align-horizontally.svg";
-  static const alignRight = "$_imgDir/align-right.svg";
-  static const linked = "$_imgDir/linked.svg";
-  static const unlinked = "$_imgDir/unlinked.svg";
-  static const clipboard = "$_imgDir/clipboard.svg";
-  static const clipboardTick = "$_imgDir/clipboard-tick.svg";
-  static const arrowRight = "$_imgDir/arrow-right.svg";
+  static const _dir = "assets/img";
+
+  static const cogWheel = "$_dir/cog-wheel.svg";
+  static const edit = "$_dir/edit.svg";
+  static const monitor = "$_dir/monitor.svg";
+  static const more = "$_dir/more.svg";
+  static const mouse = "$_dir/mouse.svg";
+  static const cross = "$_dir/cross.svg";
+  static const chevronDown = "$_dir/chevron-down.svg";
+  static const alignTop = "$_dir/align-top.svg";
+  static const alignVertically = "$_dir/align-vertically.svg";
+  static const alignBottom = "$_dir/align-bottom.svg";
+  static const alignLeft = "$_dir/align-left.svg";
+  static const alignHorizontally = "$_dir/align-horizontally.svg";
+  static const alignRight = "$_dir/align-right.svg";
+  static const linked = "$_dir/linked.svg";
+  static const unlinked = "$_dir/unlinked.svg";
+  static const clipboard = "$_dir/clipboard.svg";
+  static const clipboardTick = "$_dir/clipboard-tick.svg";
+  static const arrowRight = "$_dir/arrow-right.svg";
+}
+
+class KeyIcons {
+  KeyIcons._();
+
+  static const _dir = "assets/icons";
+  // modifiers
+  static const control = "$_dir/control.svg";
+  static const alt = "$_dir/alt.svg";
+  static const shift = "$_dir/shift.svg";
+  static const meta = "$_dir/meta.svg";
+  // normals
+  static const backspace = "$_dir/backspace.svg";
+  static const enter = "$_dir/enter.svg";
+  static const space = "$_dir/space.svg";
+  static const tab = "$_dir/tab.svg";
+  static const contextMenu = "$_dir/context-menu.svg";
+  static const printScreen = "$_dir/print-screen.svg";
+  static const pause = "$_dir/pause.svg";
+  // navigations
+  static const escape = "$_dir/escape.svg";
+  static const insert = "$_dir/insert.svg";
+  static const delete = "$_dir/delete.svg";
+  static const home = "$_dir/home.svg";
+  static const end = "$_dir/end.svg";
+  static const pageUp = "$_dir/page-up.svg";
+  static const pageDown = "$_dir/page-down.svg";
+  // arrows
+  static const arrowUp = "$_dir/arrow-up.svg";
+  static const arrowDown = "$_dir/arrow-down.svg";
+  static const arrowLeft = "$_dir/arrow-left.svg";
+  static const arrowRight = "$_dir/arrow-right.svg";
+  // locks
+  static const capsLock = "$_dir/caps-lock.svg";
+  static const capsLocked = "$_dir/caps-locked.svg";
+  static const scrollLock = "$_dir/scroll-lock.svg";
+  static const scrollLocked = "$_dir/scroll-locked.svg";
+  static const numLock = "$_dir/num-lock.svg";
+  static const numLocked = "$_dir/num-locked.svg";
+
+  // platform
+  static const macMeta = "$_dir/mac-meta.svg";
 }
