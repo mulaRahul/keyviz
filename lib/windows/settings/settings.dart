d1373e54a760850f22d658a5541079a645227bf2
Rahul Mula <96373135+mulaRahul@users.noreply.github.com>
2023-07-10 11:45:09 +0530

    🫠 about tab

--- lib/windows/settings/settings.dart
+++ lib/windows/settings/settings.dart
@@ -1,46 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:keyviz/config/config.dart';
+import 'package:keyviz/domain/vault/vault.dart';
+import 'package:keyviz/providers/key_event.dart';
 import 'package:keyviz/windows/shared/shared.dart';
 
 import 'views/views.dart';
 import 'widgets/widgets.dart';
 
-class SettingsWindow extends StatefulWidget {
+class SettingsWindow extends StatelessWidget {
   const SettingsWindow({super.key});
 
   @override
-  State<SettingsWindow> createState() => _SettingsWindowState();
+  Widget build(BuildContext context) {
+    return Selector<KeyEventProvider, bool>(
+      selector: (_, keyEvent) => keyEvent.styling,
+      builder: (_, show, __) {
+        return show ? const _SettingsWindow() : const SizedBox();
+      },
+    );
+  }
 }
 
-class _SettingsWindowState extends State<SettingsWindow> {
-  SettingsTab _currentTab = SettingsTab.general;
+class _SettingsWindow extends StatefulWidget {
+  const _SettingsWindow();
+
+  @override
+  State<_SettingsWindow> createState() => _SettingsWindowState();
+}
+
+class _SettingsWindowState extends State<_SettingsWindow> {
+  SettingsTab _currentTab = SettingsTab.about;
+  late final _positionNotifier = ValueNotifier<Offset>(const Offset(340, 260));
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 600,
-      height: 544,
-      padding: const EdgeInsets.all(defaultPadding),
-      decoration: _backgroundDecor,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          SideBar(
-            currentTab: _currentTab,
-            onChange: (tab) => setState(() => _currentTab = tab),
-          ),
-          const SmallRowGap(),
-          Expanded(
-            child: DecoratedBox(
-              decoration: _innerDecor,
-              child: SingleChildScrollView(
-                padding: const EdgeInsets.all(defaultPadding * 1.5),
-                child: _currentTabView,
+    return ValueListenableBuilder<Offset>(
+      valueListenable: _positionNotifier,
+      builder: (context, value, child) {
+        return Positioned(
+          top: value.dy,
+          left: value.dx,
+          child: child!,
+        );
+      },
+      child: Container(
+        width: 600,
+        height: 560,
+        padding: const EdgeInsets.all(defaultPadding).copyWith(
+          top: defaultPadding * .5,
+        ),
+        decoration: _backgroundDecor,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            _TopBar(
+              onPanUpdate: _onPanUpdate,
+            ),
+            const VerySmallColumnGap(),
+            Expanded(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SideBar(
+                    currentTab: _currentTab,
+                    onChange: (tab) => setState(() => _currentTab = tab),
+                  ),
+                  const SmallRowGap(),
+                  Expanded(
+                    child: DecoratedBox(
+                      decoration: _innerDecor,
+                      child: SingleChildScrollView(
+                        padding: const EdgeInsets.all(defaultPadding * 1.5),
+                        child: _currentTabView,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -53,16 +94,14 @@ class _SettingsWindowState extends State<SettingsWindow> {
       case SettingsTab.mouse:
         return const MouseTabView();
 
-      case SettingsTab.style:
+      case SettingsTab.keycap:
         return const StyleTabView();
 
       case SettingsTab.apperance:
         return const AppearanceTabView();
 
       case SettingsTab.about:
-        return const Center(
-          child: Text("Keyviz v2.0.0-alpha by Rahul Mula"),
-        );
+        return const AboutView();
     }
   }
 
@@ -70,7 +109,9 @@ class _SettingsWindowState extends State<SettingsWindow> {
         color: context.colorScheme.background,
         borderRadius: defaultBorderRadius,
         border: Border.all(
-          color: context.colorScheme.outline,
+          color: context.theme.brightness == Brightness.dark
+              ? context.colorScheme.onSecondary
+              : context.colorScheme.outline,
         ),
         boxShadow: const [
           BoxShadow(
@@ -85,4 +126,59 @@ class _SettingsWindowState extends State<SettingsWindow> {
         color: context.colorScheme.secondaryContainer,
         borderRadius: defaultBorderRadius,
       );
+
+  _onPanUpdate(DragUpdateDetails details) {
+    _positionNotifier.value = Offset(
+      _positionNotifier.value.dx + details.delta.dx,
+      _positionNotifier.value.dy + details.delta.dy,
+    );
+  }
+}
+
+class _TopBar extends StatelessWidget {
+  const _TopBar({required this.onPanUpdate});
+
+  final ValueChanged<DragUpdateDetails> onPanUpdate;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const SizedBox(),
+        GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onPanUpdate: onPanUpdate,
+          child: SizedBox(
+            width: 80,
+            height: 8,
+            child: Center(
+              child: SizedBox(
+                width: 80,
+                height: 3,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: context.colorScheme.primary.withOpacity(.2),
+                    borderRadius: BorderRadius.circular(2),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: () {
+            context.keyEvent.styling = false;
+            Vault.save(context);
+          },
+          child: SvgIcon(
+            icon: VuesaxIcons.cross,
+            size: defaultPadding * .5,
+            color: context.colorScheme.primary.withOpacity(.4),
+          ),
+        ),
+      ],
+    );
+  }
 }
---
+++ lib/windows/settings/views/about.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:keyviz/config/config.dart';
+import 'package:keyviz/windows/shared/shared.dart';
+
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+class AboutView extends StatelessWidget {
+  const AboutView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Row(
+          children: [
+            Container(
+              height: 200,
+              width: 180,
+              decoration: BoxDecoration(
+                color: context.colorScheme.primaryContainer,
+                borderRadius: defaultBorderRadius,
+                border: Border.all(color: context.colorScheme.outline),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    "assets/img/logo.svg",
+                    height: defaultPadding * 3.5,
+                  ),
+                  const SmallColumnGap(),
+                  Text(
+                    "Keyviz 2.0.0-alpha",
+                    style: context.textTheme.titleSmall,
+                  ),
+                  Text(
+                    "by Rahul Mula",
+                    style: context.textTheme.titleSmall?.copyWith(
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SmallRowGap(),
+            Expanded(
+              child: Container(
+                height: 200,
+                decoration: BoxDecoration(
+                  color: context.colorScheme.primaryContainer,
+                  borderRadius: defaultBorderRadius,
+                  border: Border.all(color: context.colorScheme.outline),
+                ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      right: 0,
+                      bottom: 0,
+                      child: SvgPicture.asset(
+                        "assets/img/keycap-grid.svg",
+                        width: defaultPadding * 9,
+                        height: defaultPadding * 9,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(
+                        defaultPadding * 1.5,
+                      ).copyWith(right: defaultPadding * 4),
+                      child: Text(
+                        "This is an alpha release, so bugs 🐛 are expected. "
+                        "If you find any bugs report the same!",
+                        style: context.textTheme.labelSmall?.copyWith(
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                    // Positioned(
+                    //   left: defaultPadding * 1.5,
+                    //   bottom: defaultPadding * 1.5,
+                    //   child: RichText(
+                    //     text: const TextSpan(
+                    //       children: [
+                    //         TextSpan(
+                    //           text: "⌨️",
+                    //           style: TextStyle(fontSize: 40),
+                    //         ),
+                    //         TextSpan(
+                    //           text: "🖱️",
+                    //           style: TextStyle(fontSize: 30),
+                    //         ),
+                    //       ],
+                    //     ),
+                    //   ),
+                    // ),
+                    Positioned(
+                      left: defaultPadding * 1.5,
+                      bottom: defaultPadding * 1.5,
+                      child: Row(
+                        children: [
+                          IconButton(
+                            onPressed: () => launchUrlString(
+                              "https://discord.gg/qyrKWCvtEq",
+                            ),
+                            tooltip: "Discord",
+                            icon: const SvgIcon(
+                              icon: "assets/img/discord-logo.svg",
+                              size: defaultPadding * .8,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () => launchUrl(
+                              Uri.parse("mailto:rahulmula10@gmail.com"),
+                            ),
+                            tooltip: "Email",
+                            icon: const SvgIcon(icon: VuesaxIcons.mail),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SmallColumnGap(),
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                height: 250,
+                decoration: BoxDecoration(
+                  color: context.colorScheme.primaryContainer,
+                  borderRadius: defaultBorderRadius,
+                  border: Border.all(color: context.colorScheme.outline),
+                ),
+                padding: const EdgeInsets.all(defaultPadding * 1.5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "💻 From Dev",
+                      style: context.textTheme.titleLarge,
+                    ),
+                    const VerySmallColumnGap(),
+                    Text(
+                      "Hi 👋, I'm Rahul Mula, the developer of Keyviz. "
+                      "I'm an instructor, and I teach courses online. \n\n"
+                      "When recording my screen, I've always felt the need "
+                      "to show my keystrokes to the audience. That's when I "
+                      "decided to develop keyviz, and share it with others "
+                      "to help people like me.",
+                      style: context.textTheme.bodyLarge,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SmallRowGap(),
+            Container(
+              height: 250,
+              width: 200,
+              decoration: BoxDecoration(
+                color: context.colorScheme.primaryContainer,
+                borderRadius: defaultBorderRadius,
+                border: Border.all(color: context.colorScheme.outline),
+              ),
+              padding: const EdgeInsets.all(defaultPadding * 1.5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "💖 Support",
+                    style: context.textTheme.titleLarge,
+                  ),
+                  const VerySmallColumnGap(),
+                  Text(
+                    "As keyviz is freeware, the only way I can earn is "
+                    "through your generous donations. It helps free my time "
+                    "and work more on keyviz.",
+                    style: context.textTheme.bodyLarge,
+                  ),
+                  const Spacer(),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () => launchUrlString(
+                          "https://github.com/sponsors/mulaRahul",
+                        ),
+                        tooltip: "Github Sponsors",
+                        icon: const SvgIcon(icon: "assets/img/github-logo.svg"),
+                      ),
+                      IconButton(
+                        onPressed: () => launchUrlString(
+                          "https://opencollective.com/keyviz",
+                        ),
+                        tooltip: "Open Collective",
+                        icon: SvgPicture.asset(
+                          "assets/img/opencollective-logo.svg",
+                          width: defaultPadding,
+                          height: defaultPadding,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
